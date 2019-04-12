b05ecefec294bdb2fcab9eddac4471f0305103a7
adruzik@elinext.com <adruzik@elinext.com>
2019-04-12 20:08:34 +0300

    Initial commit.

---
+++ src/by/bntu/fitr/povt/alexeyd/lab1/Main.java
@@ -0,0 +1,17 @@
+package by.bntu.fitr.povt.alexeyd.lab1;
+
+public class Main {
+    private static final String FIO = "Druzik Aliaksei Nikolaevich";
+    private static final String GROUP_NUMBER = "30701117";
+    private static final String ACTIVITY = "Stydying";
+    private static final String HOBBY = "Volleyball";
+    private static final String SLOGAN = "JUST DO IT";
+
+    public static void main(String[] args) {
+		System.out.println("FIO: " + FIO);
+        System.out.println("GROUP_NUMBER: " + GROUP_NUMBER);
+        System.out.println("ACTIVITY: " + ACTIVITY);
+        System.out.println("HOBBY: " + HOBBY);
+        System.out.println("SLOGAN: " + SLOGAN);
+    }
+}
