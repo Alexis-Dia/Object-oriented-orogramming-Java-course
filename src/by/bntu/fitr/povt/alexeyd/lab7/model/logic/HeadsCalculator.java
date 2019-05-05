9659dab0e223df336ecbeeabb6ebf1cfa7f69cde
Alexis-Dr <alexeydruzik@inbox.ru>
2019-05-05 19:45:35 +0300

    Fixed lab07

--- src/by/bntu/fitr/povt/alexeyd/lab7/model/logic/HeadsCalculator.java
+++ src/by/bntu/fitr/povt/alexeyd/lab7/model/logic/HeadsCalculator.java
@@ -5,12 +5,12 @@ public class HeadsCalculator {
     private final static int NUMBER_OF_HEADS_IN_FIRST_100 = 300;
     private final static int NUMBER_OF_HEADS_IN_FIRST_200 = 500;
     private final static int NUMBER_OF_HEADS_IN_NEXT_200 = 200;
+    private final static int ONE_HUNDREAD_YEARS = 100;
+    private final static int TWO_HUNDREAD_YEARS = 200;
     private final static int GROWING_SPEED_1 = 1;
     private final static int GROWING_SPEED_2 = 2;
     private final static int GROWING_SPEED_3 = 3;
     private final static int INIT_HEAD = 3;
-    private final static int ONE_HUNDREAD_YEARS = 100;
-    private final static int TWO_HUNDREAD_YEARS = 200;
     private static int numberOfHeads = INIT_HEAD;
     private static int numberOfTwoHundreds = 1;
 
@@ -20,11 +20,11 @@ public class HeadsCalculator {
             numberOfTwoHundreds = age / TWO_HUNDREAD_YEARS;
                 if (numberOfTwoHundreds ==  1) {
                     int difference = age - TWO_HUNDREAD_YEARS;
-                    numberOfHeads += calculateHeads(difference, GROWING_SPEED_1);
+                    numberOfHeads += difference * GROWING_SPEED_1;
                 } else {
                     int difference = age - numberOfTwoHundreds * TWO_HUNDREAD_YEARS;
                     numberOfHeads += numberOfTwoHundreds * TWO_HUNDREAD_YEARS;
-                    numberOfHeads += calculateHeads(difference, GROWING_SPEED_1);
+                    numberOfHeads += difference * GROWING_SPEED_1;
                     numberOfHeads -= NUMBER_OF_HEADS_IN_NEXT_200;
                 }
             numberOfHeads += NUMBER_OF_HEADS_IN_FIRST_200;
@@ -32,27 +32,11 @@ public class HeadsCalculator {
         } else if (age > ONE_HUNDREAD_YEARS && age <= TWO_HUNDREAD_YEARS) {
             int difference = age -  ONE_HUNDREAD_YEARS;
             numberOfHeads += NUMBER_OF_HEADS_IN_FIRST_100;
-            numberOfHeads += calculateHeads(difference, GROWING_SPEED_2);
+            numberOfHeads += difference * GROWING_SPEED_2;
             return numberOfHeads;
         } else
             numberOfHeads += age * GROWING_SPEED_3;
             return numberOfHeads;
-    }
-
-    private static int calculateHeads(int age, int growingSpeed) {
-        int heads = 0;
-        switch (growingSpeed) {
-            case 1:
-                heads = age * GROWING_SPEED_1;
-                break;
-            case 2:
-                heads = age * GROWING_SPEED_2;
-                break;
-            case 3:
-                heads = age * GROWING_SPEED_3;
-                break;
-        }
-        return heads;
     }
 
 }
