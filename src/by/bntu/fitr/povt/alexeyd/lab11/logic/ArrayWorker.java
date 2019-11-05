8259a42741021cb8554760c8376b3dad13d7f7c3
Alexis-Dk <alexeydruzik@inbox.ru>
2019-11-05 16:05:28 +0300

    Fixed Lab11.

--- src/by/bntu/fitr/povt/alexeyd/lab11/logic/ArrayWorker.java
+++ src/by/bntu/fitr/povt/alexeyd/lab11/logic/ArrayWorker.java
@@ -1,4 +1,32 @@
 package by.bntu.fitr.povt.alexeyd.lab11.logic;
 
 public class ArrayWorker {
+    public static int findElementByMaxModule(int[] array) {
+        int elementIndex = 0;
+        int maxAbsValue = array[elementIndex];
+        for (int i: array) {
+            if (Math.abs(i) > maxAbsValue) {
+                maxAbsValue = Math.abs(i);
+            }
+        }
+        return maxAbsValue;
+    }
+
+    public static int[] sumOfElements(int[] array) {
+        int indexOfFirstPositive = -1;
+        int indexOfSecondPositive = -1;
+        int[] positiveArr = new int[2];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (count > 2) {
+                break;
+            } else {
+                if (i > 0) {
+                    positiveArr[count] = array[i];
+                }
+            }
+            count++;
+        }
+        return positiveArr;
+    }
 }
--- src/by/bntu/fitr/povt/alexeyd/lab11/view/Printer.java
+++ src/by/bntu/fitr/povt/alexeyd/lab11/view/Printer.java
@@ -1,4 +1,7 @@
 package by.bntu.fitr.povt.alexeyd.lab11.view;
 
 public class Printer {
+    public static void print(String msg) {
+        System.out.print(msg);
+    }
 }
