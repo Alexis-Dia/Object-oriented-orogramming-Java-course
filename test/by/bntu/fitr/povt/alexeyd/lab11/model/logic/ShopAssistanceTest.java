d31ef4246523d43b64e0f9a2150b6e3713a2e424
Alexis-Dk <alexeydruzik@inbox.ru>
2019-12-19 14:21:13 +0300

    Added tests for Lab11, Lab12.

--- test/by/bntu/fitr/povt/alexeyd/lab11/model/logic/ShopAssistanceTest.java
+++ test/by/bntu/fitr/povt/alexeyd/lab11/model/logic/ShopAssistanceTest.java
@@ -1,26 +1,44 @@
 package by.bntu.fitr.povt.alexeyd.lab11.model.logic;
 
+import by.bntu.fitr.povt.alexeyd.lab11.model.entity.*;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ShopAssistanceTest {
+
+    private Bucket bucket = new Bucket("POIT30701117");
+    private ShopAssistance shopAssistance = new ShopAssistance();
 
     @Before
     public void setUp() {
 
+        bucket.addProduct(new Bread(true, 500, 1.0, true, "Borodinskiy",
+            "Chilli", "Brown", "Square", 1012234));
+        bucket.addProduct(new Orange(false, 110, 5.0, 11, 0.1,
+            true, "Rosso", "Elipse", 201243));
+        bucket.addProduct(new Milk(false, 450, 3.0, 500, 3.2, 7.7,
+            false, true, 45134343));
     }
 
     @Test
     public void calculateAvgPrice() {
+        double result = shopAssistance.calculateAvgPrice(bucket);
+        assertEquals(3, result, 0.001);
     }
 
     @Test
     public void calculateAvgWeight() {
+        double result = shopAssistance.calculateAvgWeight(bucket);
+        assertEquals(1060.0, result, 0);
     }
 
     @Test
     public void getPrize() {
+        Product product = bucket.getContainer()[0];
+        assertTrue(CoreMatchers.is(product.isHelpChildren()).matches(true));
     }
 }
--- test/by/bntu/fitr/povt/alexeyd/lab12/model/logic/ShopAssistanceTest.java
+++ test/by/bntu/fitr/povt/alexeyd/lab12/model/logic/ShopAssistanceTest.java
@@ -1,24 +1,47 @@
 package by.bntu.fitr.povt.alexeyd.lab12.model.logic;
 
+import by.bntu.fitr.povt.alexeyd.lab12.model.entity.*;
+import by.bntu.fitr.povt.alexeyd.lab12.util.SubGroup;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ShopAssistanceTest {
+
+    private Bucket bucket = new Bucket("POIT30701117");
+    private ShopAssistance shopAssistance = new ShopAssistance();
 
     @Before
     public void setUp() {
 
+        bucket.addProduct(new Bread(true, 500, 1.2, true, "Borodinskiy",
+            "Chilli", "Brown", "Square", 1276234), SubGroup.BREAD.getGroupCode());
+        bucket.addProduct(new Orange(false, 110, 0.7, 11, 0.1,
+            true, "Rosso", "Elipse", 73483438), SubGroup.FRUIT.getGroupCode());
+        bucket.addProduct(new Milk(false, 450, 2.1, 500, 3.2, 7.7,
+            false, true, 32635623), SubGroup.MILK.getGroupCode());
+        bucket.addProduct(new Milk(false, 450, 2.3, 500, 5.2, 9.7,
+            false, true, 32635624), SubGroup.MILK.getGroupCode());
     }
 
     @Test
     public void calculateAvgPrice() {
+        double result = shopAssistance.calculateAvgPrice(bucket);
+        assertEquals(1.575, result, 0.001);
     }
 
     @Test
     public void calculateAvgWeight() {
+        double result = shopAssistance.calculateAvgWeight(bucket);
+        assertEquals(1510.0, result, 0);
     }
 
     @Test
     public void getPrize() {
+        Product product = bucket.getContainer()[SubGroup.FRUIT.getGroupCode()][0];
+        assertTrue(CoreMatchers.is(product.isHelpChildren()).matches(true));
     }
 }
