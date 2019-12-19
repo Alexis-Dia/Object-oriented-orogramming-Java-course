a9fc1377058c747dd26d224db3cb30cf1113c599
Alexis-Dk <alexeydruzik@inbox.ru>
2019-12-19 10:23:31 +0300

    Initialized tests for Lab11, Lab12.

---
+++ test/by/bntu/fitr/povt/alexeyd/lab11/model/logic/ShopAssistanceTest.java
@@ -0,0 +1,26 @@
+package by.bntu.fitr.povt.alexeyd.lab11.model.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShopAssistanceTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void calculateAvgPrice() {
+    }
+
+    @Test
+    public void calculateAvgWeight() {
+    }
+
+    @Test
+    public void getPrize() {
+    }
+}
---
+++ test/by/bntu/fitr/povt/alexeyd/lab12/model/logic/ShopAssistanceTest.java
@@ -0,0 +1,24 @@
+package by.bntu.fitr.povt.alexeyd.lab12.model.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShopAssistanceTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void calculateAvgPrice() {
+    }
+
+    @Test
+    public void calculateAvgWeight() {
+    }
+
+    @Test
+    public void getPrize() {
+    }
+}
