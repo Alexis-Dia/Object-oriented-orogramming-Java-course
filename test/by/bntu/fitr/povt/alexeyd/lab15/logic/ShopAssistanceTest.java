58842776b0754bfc737cd2409b29025c4420a0dd
Alexis-Dr <alexeydruzik@inbox.ru>
2019-12-24 23:56:05 +0300

    Added jUnit tests for ShopAssistance.

---
+++ test/by/bntu/fitr/povt/alexeyd/lab15/logic/ShopAssistanceTest.java
@@ -0,0 +1,40 @@
+package by.bntu.fitr.povt.alexeyd.lab15.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShopAssistanceTest {
+
+    @Test
+    public void calculateAvgPrice() {
+    }
+
+    @Test
+    public void calculateAvgWeight() {
+    }
+
+    @Test
+    public void getPrize() {
+    }
+
+    @Test
+    public void findByPrice() {
+    }
+
+    @Test
+    public void findByWeight() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void serializeData() {
+    }
+
+    @Test
+    public void parseProduct() {
+    }
+}
