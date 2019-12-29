151c957d226bf4e2da578fbe6d94c6db906b14b3
Alexis-Dr <alexeydruzik@inbox.ru>
2019-12-29 20:52:25 +0300

    Fixed Lab16.

--- src/by/bntu/fitr/povt/alexeyd/lab16/factory/MyDataStoreFactory.java
+++ src/by/bntu/fitr/povt/alexeyd/lab16/factory/MyDataStoreFactory.java
@@ -2,19 +2,33 @@ package by.bntu.fitr.povt.alexeyd.lab16.factory;
 
 import by.bntu.fitr.povt.alexeyd.lab16.utils.Constant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static by.bntu.fitr.povt.alexeyd.lab15.logic.Constant.*;
 
 public class MyDataStoreFactory extends DataStoreFactory {
 
+    private static final Map<String, DataGenerator> GENERATORS_MAP;
+
+    static {
+        GENERATORS_MAP = new HashMap<>();
+        GENERATORS_MAP.put(Constant.RANDOM, new RandomDataGenerator());
+        GENERATORS_MAP.put(Constant.TEXT, new TextDataGenerator());
+        GENERATORS_MAP.put(Constant.BINARY, new BinaryDataGenerator());
+        GENERATORS_MAP.put(Constant.BINARY_SERIALIZABLE, new SerializableBinaryDataGenerator());
+    }
+
+
     DataGenerator runFactory(String item) {
         if (item.equals(RANDOM)) {
-            return new RandomDataGenerator();
+            return GENERATORS_MAP.get(Constant.RANDOM);
         } else if (item.equals(TEXT)) {
-            return new TextDataGenerator();
+            return GENERATORS_MAP.get(Constant.TEXT);
         } else if (item.equals(BINARY)) {
-            return new BinaryDataGenerator();
+            return GENERATORS_MAP.get(Constant.BINARY);
         } else if(item.equals(Constant.BINARY_SERIALIZABLE) || item.equals(Constant.CUSTOM_BINARY_SERIALIZABLE)) {
-            return new SerializableBinaryDataGenerator();
+            return GENERATORS_MAP.get(Constant.BINARY_SERIALIZABLE);
         } else return null;
     }
 }
