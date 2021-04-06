065f0617d65357a6a582cb1d6a3aee84a398e1cf
Alexis-Dk <alexeydruzik@inbox.ru>
2021-04-06 20:42:07 +0300

    Fixed Lab20.

--- src/by/bntu/fitr/povt/alexeyd/lab20/Bus.java
+++ src/by/bntu/fitr/povt/alexeyd/lab20/Bus.java
@@ -2,7 +2,6 @@ package by.bntu.fitr.povt.alexeyd.lab20;
 
 import org.apache.log4j.Logger;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Semaphore;
 
 import static by.bntu.fitr.povt.alexeyd.lab20.Lab20MainTask.MAX_CAPACITY_NUMBER_OF_BUSES_IN_THE_ONE_STATION;
@@ -17,14 +16,12 @@ public class Bus implements Runnable {
     private String backPlateNumber;
     public static final String MY_SUPER_LOGGER = "MySuperLogger";
     private static final Logger LOG = Logger.getLogger(MY_SUPER_LOGGER);
-    private final ExecutorService executor;
 
-    public Bus(int occupyingTime, BusStation busStation, String backPlateNumber, ExecutorService executor) {
+    public Bus(int occupyingTime, BusStation busStation, String backPlateNumber) {
         this.occupyingTime = occupyingTime;
         this.busStation = busStation;
         this.backPlateNumber = backPlateNumber;
         thread = new Thread(this);
-        this.executor = executor;
         thread.start();
     }
 
--- src/by/bntu/fitr/povt/alexeyd/lab20/Lab20MainTask.java
+++ src/by/bntu/fitr/povt/alexeyd/lab20/Lab20MainTask.java
@@ -2,11 +2,6 @@ package by.bntu.fitr.povt.alexeyd.lab20;
 
 import by.bntu.fitr.povt.alexeyd.lab20.utils.BusStationUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
  * https://habr.com/ru/post/277669/
  * Number of laboratory work is №20. Java Threads. Synchronization. The Singleton Pattern.
@@ -21,22 +16,13 @@ public class Lab20MainTask {
 
     public static void main(String[] args) {
 
-        List<ExecutorService> executorServicelist = new ArrayList<>();
         for (int i = 0; i < MAX_CAPACITY_NUMBER_OF_BUSES_IN_THE_ONE_STATION; i++) {
-            ExecutorService executor = Executors.newCachedThreadPool();
-            executorServicelist.add(executor);
-        }
-
-        for (int i = 0; i < executorServicelist.size(); i++) {
             BusStation busStation = new BusStation(Integer.toString(i + 1));
-            final int finalI = i;
-            executorServicelist.get(i).execute(() -> {
-                for (int j = 0; j < NUMBER_OF_BUSES; j++) {
-                    int occupyingTime = BusStationUtils.generateRandomOccupyingTimeOfParking();
-                    String backPlateNumber = BusStationUtils.generateCarBackplateNumber();
-                    new Bus(occupyingTime, busStation, backPlateNumber, executorServicelist.get(finalI));
-                }
-            });
+            for (int j = 0; j < NUMBER_OF_BUSES; j++) {
+                int occupyingTime = BusStationUtils.generateRandomOccupyingTimeOfParking();
+                String backPlateNumber = BusStationUtils.generateCarBackplateNumber();
+                new Bus(occupyingTime, busStation, backPlateNumber);
+            }
         }
 
     }
